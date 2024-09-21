de80a7ecb1e89d62d74dd935b1e342e84520af93
developerpuja <pujas2120@gmail.com>
2024-09-21 12:37:40 +0530

    Initial commit

---
+++ src/main/java/com/datastructure_practice/DatastructurePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.datastructure_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructurePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructurePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datastructure_practice/DatastructurePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datastructure_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructurePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
